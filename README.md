06441c6adb3a336fcb033e24ec9f41314c500a57
johanez <johanez@users.noreply.github.com>
2021-02-07 17:47:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Buttondown Newsletter Plugin for [BluditCMS](www.blufit.com)
+
+[Buttondown.email](https://buttondown.email/) is a newsletter service that I prefer over other like e.g. MailChimp.
+I'm not affiliated with Buttondown in any other way then happily using there service.
+
+## Instalation
+
+Download and extract into `bl-blugins`.
+
+## Usage
+
+- Create a [Buttondown.email](https://buttondown.email/) account and configue it for your website.
+- Copy the API key into the plugin settings.
+
+### How it works
+
+- Emails will be sent for any that full fills the following criteria:
+  - not static
+  - not scheduled
+  - published
+  - creation date after the start date in the plug settings
+  - `no-index` option OFF (in page SEO settings, this can be used for excluding pages from the newsletter, e.g. for testing)
+  - not scheduled
+  - no newsletter for this page has been sent before. **For exceptions see _warnings_ below!.**
+- An email will be sent whenever a page is saved for the first time matching these criteria. This is normally the initial creation, or saving a draft page as published.
+- The mail body contains the cover image (optionally), the page title and the content (including html).
+
+### Warnings
+
+- **Changing the slug of a page (friendly URL in page SEO settings) will cause a resend!**
+- If the plugin is _paused_ (on this plugin's settings page), mails will not be send. **However, when active again, mails will be sent for pages that were created while paused, when these pages are modified and saved matching the above criteria.**
+- If the plugin is deactivated and reactivated from the Bludit admin plugin page, the sent list will be cleared. This means mails will be sent again for any page modified matching the criteria!
+
+### Known Issues
+
+- A newsletter _should_ be sent when a a scheduled page is due and appears on the site. However, the internal trigger for this doesn't seem to work. You can (messily) patch this by adding `Theme::plugins('afterPageModify', array($pageKey));` in the `scheduler()` before `$saveDatabase = true;` in the file `pages.class.php`.
+
+## License
+
+MIT, see LICENSE file.
---
+++ plugin.php
@@ -0,0 +1,187 @@
+<?php
+
+class buttondown_newsletter extends Plugin {
+	
+
+	public function init()
+	{
+		global $site;
+		// Fields and default values for the database of this plugin
+		$this->dbFields = array(
+			'apiKey'=>'', // APIKey form buttondown
+			'active'=> true,
+			'startDate' => date('Y-m-d H:i:s'), // page creation date which newsletter will be send YYYY-MM-DD Hours:Minutes:Seconds
+			'sentEmails'=> json_encode(array()),
+			'includeCover' => false,
+			'subjectPrefix' => 'New post on '.$site->title().': ',
+		);
+	}
+
+	  // Method/function to process the post settings.
+	public function post()
+	{
+
+	// Process the POST variables like in the parent function.
+	// Code from file: bludit/bl-kernel/abstract/plugin.class.php
+	$args = $_POST;
+	foreach ($this->dbFields as $key=>$value) {
+		if (isset($args[$key])) {
+			$value = Sanitize::html($args[$key]);
+			if ($value === 'false') {
+			$value = false;
+			} elseif ($value === 'true') {
+			$value = true;
+			}
+			settype($value, gettype($this->dbFields[$key]));
+			$this->db[$key] = $value;
+		}
+	}
+
+	// Save the plugin settings to the database.
+	return $this->save();
+
+	}
+  
+
+	// Method called on the settings of the plugin on the admin area
+	public function form()
+	{
+		global $L;
+		$sentEmails = $this->getSentEmailsArray();
+
+		$html  = '<div class="alert alert-primary" role="alert">';
+		$html .= $this->description();
+		$html .= '</div>';
+
+		$html .= '<div>';
+		// API key
+		$html .= '<label><strong>Buttondown API Key</strong></label>';
+		$html .= '<input id="apiKey" name="apiKey" type="text" value="'.$this->getValue('apiKey').'">';
+		$html .= '<span class="tip">Copy your API key on https://buttondown.email/settings/programming </span>';
+		// Sending active
+		$html .= '<label>'.$L->get('active').'</label>';
+		$html .= '<select id="active" name="active" type="text">';
+		$html .= '<option value="true" '.($this->getValue('active')===true?'selected':'').'>'.$L->get('on').'</option>';
+		$html .= '<option value="false" '.($this->getValue('active')===false?'selected':'').'>'.$L->get('off').'</option>';
+		$html .= '</select>';
+		$html .= '<span class="tip">'.$L->get('active-tip').'</span>';
+
+		// Start date
+		$html .= '<label>'.$L->get('send-after').'</label>';
+		$html .= '<input id="startDate" name="startDate" type="text" value="'.$this->getValue('startDate').'">';
+		$html .= '<span class="tip">'.$L->get('send-after-tip').'</span>';
+		// Subject Prefix
+		$html .= '<label>'.$L->get('subject-prefix').'</label>';
+		$html .= '<input id="subjectPrefix" name="subjectPrefix" type="text" value="'.$this->getValue('subjectPrefix').'">';
+		$html .= '<span class="tip">'.$L->get('subject-prefix-tip').'</span>';
+				// Sending active
+		$html .= '<label>'.$L->get('include-cover').'</label>';
+		$html .= '<select id="includeCover" name="includeCover" type="text">';
+		$html .= '<option value="true" '.($this->getValue('includeCover')===true?'selected':'').'>'.$L->get('on').'</option>';
+		$html .= '<option value="false" '.($this->getValue('includeCover')===false?'selected':'').'>'.$L->get('off').'</option>';
+		$html .= '</select>';
+		$html .= '<span class="tip">'.$L->get('include-cover-tip').'</span>';
+		$html .= '</div><hr>';
+		// List of page keys for which mail was sent 
+		$html .= '<h4>'.$L->get('sent-list').'</h4>';
+		$html .= '<span class="tip">'.$L->get('sent-list-tip').'</span>';
+		$html .= '<div style="overflow-y: scroll; height:400px;"><ul>';
+		foreach ($sentEmails as $sentKey):
+			$html .= '<li>'.$sentKey.'</li>';
+		endforeach;
+		$html .= '</ul></div>';
+		$html .= '<div><a target="_blank" rel="noopener" href="https://buttondown.email/archive">Buttondown Archive</a></div>';
+		return $html;
+	}
+
+	// Get array of sent email keys
+	public function getSentEmailsArray() 
+	{
+		$sentEmailsJSON = $this->getValue('sentEmails',false);
+		$sentEmails = json_decode($sentEmailsJSON, true);
+		return $sentEmails;
+	}
+
+
+	public function sendEmail($key)
+	{
+		if ($this->getValue('active')){
+			$apiKey = $this->getValue('apiKey'); 
+			$sentEmails = $this->getSentEmailsArray();
+			$url  = 'https://api.buttondown.email/v1/emails';
+			$page = new Page($key);
+			$startDateTS = strtotime($this->getValue('startDate'));
+			$pageDateTS = strtotime($page->dateRaw()); 
+			global $syslog;
+			$syslog->add(array(
+				'dictionaryKey'=>'buttondown-trigger',
+				'notes'=>$key.'_'.$page->dateRaw().'_'.($startDateTS<=$pageDateTS===true?'startdatesmaller':'startdatebigger'),
+				'is_published'=>$page->published()
+			));
+
+			// Only if no newslettetr was sent and page is NOT static, ausosave, nonindex
+			if ($page->published() and 
+					!$page->isStatic() and 
+					!$page->autosave() and
+					!$page->noindex() and
+					$startDateTS <= $pageDateTS and
+					!in_array($key, $sentEmails)){
+
+				$body = '<h1>'.$page->title().'</h1>'.$page->content();
+				if($page->coverImage() and $this->getValue('includeCover')){
+					$body = '<img class="teaser" src="'.$page->coverImage().'">'.$body;
+				}
+			
+				$data = array(
+					'body' => $body,
+					'secondary_id' => $page->position(),
+					'subject' => $this->getValue('subjectPrefix').$page->title(),
+					'external_url' => $page->permalink(),
+					'slug' => $page->slug(),
+				);
+				// use key 'http' even if you send the request to https://...
+				$options = array(
+					'http' => array(
+						'header'  => "Authorization: Token {$apiKey}\r\n".
+						"Content-Type: application/json\r\n",
+						'method'  => 'POST',
+						'content' => json_encode($data)
+					)
+				);
+				$context  = stream_context_create($options);
+				$syslog->add(array(
+					'dictionaryKey'=>'buttondown-prep',
+					'notes'=>json_encode($data)
+				));
+				$result = file_get_contents($url, false, $context);
+				if ($result === FALSE) { /* Handle error */ } else {
+					// Add key to lsit of sent emails
+					array_push($sentEmails, $key);
+					$syslog->add(array(
+						'dictionaryKey'=>'buttondown-sent',
+						'notes'=>$key
+					));
+					$this->db['sentEmails'] = json_encode($sentEmails);
+					var_dump($result);
+					return $this->save();
+				}
+				
+				var_dump($result);
+			}
+		}
+		return false;
+	}
+
+	public function afterPageCreate($key)
+	{
+		$this -> sendEmail($key);
+	}
+
+	public function afterPageModify($key)
+	{
+		$this -> sendEmail($key);
+	}
+
+}
+
+?>
